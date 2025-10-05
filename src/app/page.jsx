3bb4f4e7b78668eae7174b73728065c62bbfb6a7
Yash-Waghmare <yashwaghmare9007@gmail.com>
2025-10-05 08:43:20 +0530

    about section added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import "./globals.css";
+import About from "@/components/about";
 
 export default function Home() {
   return (
     <>
-      <h1>hello world</h1>
+      <About />
     </>
   );
 }
